eeabf592eb1124310c432f64e55a715fce7f5054
Ari Epstein <aepstein607@gmail.com>
2019-10-19 08:44:23 -0400

    Add heroku configuration.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const visits = require('./routes/api/visits')
 
@@ -16,6 +17,13 @@ mongoose
     .catch(err => console.log(err));
 
 app.use('/api/visits', visits);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
