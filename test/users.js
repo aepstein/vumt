5c5598019951bcbd5ce32599ccb9600d9e674bca
Ari Epstein <aepstein607@gmail.com>
2019-11-23 22:09:37 -0500

    Move users tests to async/await syntax.

--- test/users.js
+++ test/users.js
@@ -12,89 +12,62 @@ describe('Users', () => {
         await User.deleteMany({});        
     });
     describe('POST /api/users',() => {
-        it('should register valid user', (done) => {
-            let user = validUser();
-            chai.request(server)
+        let action = async (user) => {
+            return chai.request(server)
                 .post('/api/users')
-                .send(user)
-                .end((err,res) => {
-                    res.should.have.status(201);
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('user');
-                    res.body.user.should.have.property('id');
-                    res.body.user.should.have.property('firstName').eql(user.firstName);
-                    res.body.user.should.have.property('lastName').eql(user.lastName);
-                    res.body.should.have.property('token');
-                });
-            done();
+                .send(user);            
+        }
+        it('should register valid user', async () => {
+            const user = validUser();
+            res = await action(user);
+            res.should.have.status(201);
+            res.body.should.be.a('object');
+            res.body.should.have.property('user');
+            res.body.user.should.have.property('id');
+            res.body.user.should.have.property('firstName').eql(user.firstName);
+            res.body.user.should.have.property('lastName').eql(user.lastName);
+            res.body.should.have.property('token');
         });
-        it('should not register without firstName', (done) => {
+        it('should not register without firstName', async () => {
             let user = validUser();
             delete user.firstName;
-            chai.request(server)
-                .post('/api/users')
-                .send(user)
-                .end((err,res) => {
-                    res.should.have.status(400);
-                    res.body.should.be.a('object');
-                    res.body.should.have.a.property('msg').eql('Please enter required fields')
-                });
-            done();
+            res = await action(user);
+            await res.should.have.status(400);
+            await res.body.should.be.a('object');
+            await res.body.should.have.a.property('msg').eql('Please enter required fields');
         });
-        it('should not register without lastName', (done) => {
+        it('should not register without lastName', async () => {
             let user = validUser();
             delete user.lastName;
-            chai.request(server)
-                .post('/api/users')
-                .send(user)
-                .end((err,res) => {
-                    res.should.have.status(400);
-                    res.body.should.be.a('object');
-                    res.body.should.have.a.property('msg').eql('Please enter required fields')
-                });
-            done();
+            res = await action(user);
+            await res.should.have.status(400);
+            await res.body.should.be.a('object');
+            await res.body.should.have.a.property('msg').eql('Please enter required fields');
         });
-        it('should not register without email', (done) => {
+        it('should not register without email', async () => {
             let user = validUser();
             delete user.email;
-            chai.request(server)
-                .post('/api/users')
-                .send(user)
-                .end((err,res) => {
-                    res.should.have.status(400);
-                    res.body.should.be.a('object');
-                    res.body.should.have.a.property('msg').eql('Please enter required fields')
-                });
-            done();
+            res = await action(user);
+            await res.should.have.status(400);
+            await res.body.should.be.a('object');
+            await res.body.should.have.a.property('msg').eql('Please enter required fields');
         });
-        it('should not register without password', (done) => {
+        it('should not register without password', async () => {
             let user = validUser();
             delete user.password;
-            chai.request(server)
-                .post('/api/users')
-                .send(user)
-                .end((err,res) => {
-                    res.should.have.status(400);
-                    res.body.should.be.a('object');
-                    res.body.should.have.a.property('msg').eql('Please enter required fields')
-                });
-            done();
+            res = await action(user);
+            await res.should.have.status(400);
+            await res.body.should.be.a('object');
+            await res.body.should.have.a.property('msg').eql('Please enter required fields');
         });
-        it('should not register with duplicate email', (done) => {
+        it('should not register with duplicate email', async () => {
             let user = validUser();
             const originalUser = new User(user);
-            originalUser.save()
-                .then((user) => {
-                    chai.request(server)
-                    .post('/api/users')
-                    .send(user)
-                    .end((err,res) => {
-                        res.should.have.status(400);
-                        res.body.should.be.a('object');
-                        res.body.should.have.a.property('msg').eql('User already exists with that email')
-                    });
-                    done();
-                });
+            await originalUser.save();
+            res = await action(user);
+            await res.should.have.status(400);
+            await res.body.should.be.a('object');
+            await res.body.should.have.a.property('msg').eql('User already exists with that email');
         });
     });
 });
