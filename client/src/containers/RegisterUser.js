eff6d3e7f87c3b42527f1326790c14dfe9f57df1
Ari Epstein <aepstein607@gmail.com>
2019-12-25 06:36:37 -0500

    Refactor RegisterUser to React hooks from class component.

--- client/src/containers/RegisterUser.js
+++ client/src/containers/RegisterUser.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import {
     Button,
@@ -9,133 +9,121 @@ import {
     Input,
     Alert
 } from 'reactstrap';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { register } from '../actions/authActions';
 import { clearErrors } from '../actions/errorActions';
 
-class RegisterUser extends Component {
-    state = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        msg: null
-    }
+function RegisterUser() {
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+    const error = useSelector(state => state.error)
 
-    static propTypes = {
-        isAuthenticated: PropTypes.bool,
-        error: PropTypes.object.isRequired,
-        register: PropTypes.func.isRequired,
-        clearErrors: PropTypes.func.isRequired
-    }
+    const dispatch = useDispatch()
+    const [ firstName, setFirstName ] = useState('')
+    const [ lastName, setLastName ] = useState('')
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
+    const [ msg, setMsg ] = useState(null)
 
-    componentDidUpdate(prevProps) {
-        const { error, isAuthenticated } = this.props;
-        if (error !== prevProps.error) {
-            if (error.id === 'REGISTER_FAIL') {
-                this.setState({msg: error.msg.msg});
-            } else {
-                this.setState({msg: null});
-            }
-        }
-        if (isAuthenticated) {
-            this.closeSuccessfully();
-        }
+    // componentDidUpdate(prevProps) {
+    //     if (error !== prevProps.error) {
+    //         if (error.id === 'REGISTER_FAIL') {
+    //             setMsg(error.msg.msg);
+    //         } else {
+    //             setMsg(null);
+    //         }
+    //     }
+    //     if (isAuthenticated) {
+    //         this.closeSuccessfully();
+    //     }
+    // }
+    // const closeSuccessfully = () => {
+    //     dispatch(clearErrors())
+    // }
+    const onChange = (setter) => (e) => {
+        setter(e.target.value)
     }
-
-    closeSuccessfully = () => {
-        this.props.clearErrors();
-    }
-    onChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
-    }
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        const {
-            firstName,
-            lastName,
-            email,
-            password } = this.state;
         const newUser = {
             firstName,
             lastName,
             email,
             password
         }
-        this.props.register(newUser);
+        dispatch(clearErrors())
+        dispatch(register(newUser))
     }
-    render() {
-        return(
-            <div>
-                { this.props.isAuthenticated ? <Redirect to="/" /> : null }
-                <Container>
-                    <h2>Register</h2>
-                    {this.state.msg ? <Alert color="danger">{this.state.msg}</Alert> : null }
-                    <Form
-                        onSubmit={this.onSubmit}
-                    >
-                        <FormGroup>
-                            <Label for="firstName">First Name</Label>
-                            <Input
-                                type="text"
-                                name="firstName"
-                                id="firstName"
-                                placeholder="First Name"
-                                onChange={this.onChange}
-                                className="mb-3"
-                            />
-                            <Label for="lastName">Last Name</Label>
-                            <Input
-                                type="text"
-                                name="lastName"
-                                id="lastName"
-                                placeholder="Last Name"
-                                onChange={this.onChange}
-                                className="mb-3"
-                            />
-                            <Label for="email">Email</Label>
-                            <Input
-                                type="email"
-                                name="email"
-                                id="email"
-                                placeholder="address@example.com"
-                                onChange={this.onChange}
-                                className="mb-3"
-                            />
-                            <Label for="password">Password</Label>
-                            <Input
-                                type="password"
-                                name="password"
-                                id="password"
-                                placeholder="Password"
-                                onChange={this.onChange}
-                                className="mb-3"
-                            />
-                            <Button
-                                color="dark"
-                                style={{marginTop: '2rem'}}
-                                block
-                            >Register</Button>
-                        </FormGroup>
-                    </Form>
-                </Container>
-            </div>
-        );
-    }
+
+    useEffect(() => {
+        if (error.id === 'REGISTER_FAIL') {
+            setMsg(error.msg.msg)
+        } else {
+            setMsg(null)
+        }
+    }, [error] )
+
+    return <div>
+        { isAuthenticated ? <Redirect to="/" /> : null }
+        <Container>
+            <h2>Register</h2>
+            {msg ? <Alert color="danger">{msg}</Alert> : null }
+            <Form
+                onSubmit={onSubmit}
+            >
+                <FormGroup>
+                    <Label for="firstName">First Name</Label>
+                    <Input
+                        type="text"
+                        name="firstName"
+                        id="firstName"
+                        placeholder="First Name"
+                        onChange={onChange(setFirstName)}
+                        className="mb-3"
+                    />
+                    <Label for="lastName">Last Name</Label>
+                    <Input
+                        type="text"
+                        name="lastName"
+                        id="lastName"
+                        placeholder="Last Name"
+                        onChange={onChange(setLastName)}
+                        className="mb-3"
+                    />
+                    <Label for="email">Email</Label>
+                    <Input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="address@example.com"
+                        onChange={onChange(setEmail)}
+                        className="mb-3"
+                    />
+                    <Label for="password">Password</Label>
+                    <Input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="Password"
+                        onChange={onChange(setPassword)}
+                        className="mb-3"
+                    />
+                    <Button
+                        color="dark"
+                        style={{marginTop: '2rem'}}
+                        block
+                    >Register</Button>
+                </FormGroup>
+            </Form>
+        </Container>
+    </div>
 }
 
-const mapStateToProps = (state) => ({
-    isAuthenticated: state.auth.isAuthenticated,
-    error: state.error
-})
+RegisterUser.propTypes = {
+    isAuthenticated: PropTypes.bool,
+    error: PropTypes.object.isRequired,
+    register: PropTypes.func.isRequired,
+    clearErrors: PropTypes.func.isRequired
+}
 
-export default connect(
-    mapStateToProps,
-    {
-        register,
-        clearErrors
-    }
-)(RegisterUser);
+export default RegisterUser;
