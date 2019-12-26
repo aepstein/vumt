95ca594274d63d608db3ade355101e2bc34707f0
Ari Epstein <aepstein607@gmail.com>
2019-12-26 17:27:34 -0500

    Refactor UserDashboard to hooks.

--- client/src/containers/UserDashboard.js
+++ client/src/containers/UserDashboard.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container } from 'reactstrap';
 import './UserDashboard.css';
 import VisitsList from './VisitsList';
 import VisitModal from './VisitModal';
 
-class UserDashboard extends Component {
-  render() {
-    return (
-      <div className="UserDashboard">
-        <Container>
-            <VisitModal />
-            <VisitsList />
-        </Container>
-      </div>
-    );
-  }
+function UserDashboard() {
+  return <div className="UserDashboard">
+    <Container>
+        <VisitModal />
+        <VisitsList />
+    </Container>
+  </div>
 }
 
 export default UserDashboard
