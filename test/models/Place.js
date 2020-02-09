8234ba01cc2e196f49e9a23c3188ab95ecaf179f
Ari Epstein <aepstein607@gmail.com>
2020-02-09 14:26:18 -0500

    Add place model validation for timezone to limit to IANA. Provide script to update valid zone list from package.

--- test/models/Place.js
+++ test/models/Place.js
@@ -1,7 +1,12 @@
 const { factory } = require('../setup')
+const ValidationError = require('mongoose/lib/error/validation')
 
 describe('Place', () => {
-    it('creates a valid place', () => {
-        return factory.create('place')
+    it('creates a valid place', async () => {
+        await factory.create('place')
+    })
+    it('should not save with an invalid timezone', async () => {
+        const place = await factory.build('place',{timezone: 'America/Whoville'})
+        await place.save().should.eventually.be.rejectedWith(ValidationError)
     })
 })
