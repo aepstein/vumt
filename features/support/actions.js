9e276644e5f7dc96ff566e55fd98d8adee9dbfca
Ari Epstein <aepstein607@gmail.com>
2020-02-11 20:52:08 -0500

    Re-arrange action methods alphabetically.

--- features/support/actions.js
+++ features/support/actions.js
@@ -5,6 +5,20 @@ const { toLocalDate } = require('../../test/support/util')
 const User = require('../../models/User')
 var sc = 1;
 
+const clearInput = async (el) => {
+	await el.evaluate((input) => input.value = "")
+}
+// credit: https://gist.github.com/tokland/d3bae3b6d3c1576d8700405829bbdb52
+// see: https://stackoverflow.com/questions/47407791/puppeteer-click-on-element-with-text
+const clickByText = async (text, context = "//a") => {
+	const escapedText = escapeXpathString(text);
+	const selector = `${context}[contains(text(), ${escapedText})]`;
+	await clickByXPath(selector)
+}
+const clickByXPath = async (selector) => {
+	const el = await scope.context.currentPage.$x(selector);
+	await el[0].click();
+}
 const create = async (template, attrs={}) => {
 	if (!(template in scope.models)) {
 		scope.models[template] = []
@@ -13,90 +27,12 @@ const create = async (template, attrs={}) => {
 	scope.models[template].push(created)
 	return created
 }
-const userExists = async (attr) => {
-	scope.context.user = await scope.factory.create('user',{password: "secret", ...attr});
-}
-const visitExists = async (attr={}) => {
-    scope.context.visit = await scope.factory.create('visit',attr);
-}
-const loginAs = async (email) => {
-    await clickByText("Login");
-    await fillByLabel("Email",email);
-    await fillByLabel("Password","secret");
-    await clickByText("Login","//button");
-}
-
-const initPage = async () => {
-	if ( scope.context.currentPage ) return
-	scope.context.currentPage = await scope.browser.newPage();
-	scope.context.currentPage.setViewport({ width: 1280, height: 1024 });
-	scope.context.currentPage.on('error', err => {
-		console.log("browser error: ",err);
-	});
-	scope.context.currentPage.on('pageerror', err => {
-		console.log("browser pageerror: ",err);
-	});
-}
-
-const visitPage = async (path) => {
-	await initPage()
-	const url = scope.host + paths[path];
-	const visit = await scope.context.currentPage.goto(url, {
-		waitUntil: 'networkidle2'
-	});
-	await waitFor('.navbar')
-	return visit;
-};
-
 // credit: https://gist.github.com/tokland/d3bae3b6d3c1576d8700405829bbdb52
 // see: https://stackoverflow.com/questions/47407791/puppeteer-click-on-element-with-text
 const escapeXpathString = str => {
 	const splitedQuotes = str.replace(/'/g, `', "'", '`);
 	return `concat('${splitedQuotes}', '')`;
-};
-const shouldBeLoggedInAs = async(email) => {
-	await waitFor("//a[contains(text(),'Logout')]");
-	const user = await User.findOne({email})
-    await shouldSeeText(".navbar",false,`Welcome, ${user.firstName}`);
-    await shouldSeeText(".navbar",false,"Logout");
 }
-const shouldSee = async (selector,context) => {
-	await scope.context.currentPage.waitForXPath(selectors[context][selector]);
-}
-  
-const shouldSeeText = async (selector, not, expectedText) => {
-	const elementText = await scope.context.currentPage.$eval(selector, el => el.textContent);
-    const containsText = elementText && elementText.replace(/\s+/,' ')
-	const shouldContainText = not ? false : true;
-	if (shouldContainText) {
-		containsText.should.have.string(expectedText)
-	}
-	else {
-		containsText.should.not.have.string(expectedText)
-	}
-}
-
-// credit: https://gist.github.com/tokland/d3bae3b6d3c1576d8700405829bbdb52
-// see: https://stackoverflow.com/questions/47407791/puppeteer-click-on-element-with-text
-const clickByText = async (text, context = "//a") => {
-	const escapedText = escapeXpathString(text);
-	const selector = `${context}[contains(text(), ${escapedText})]`;
-	await clickByXPath(selector)
-};
-const clickByXPath = async (selector) => {
-	const el = await scope.context.currentPage.$x(selector);
-	await el[0].click();
-}
-const waitForText = async (text, context = "//a") => {
-	const escapedText = escapeXpathString(text);
-	const selector = `${context}[contains(text(), ${escapedText})]`;
-	await scope.context.currentPage.waitForXPath(selector);
-};
-
-const clearInput = async (el) => {
-	await el.evaluate((input) => input.value = "")
-}
-
 const fillByLabel = async (label, fill ) => {
 	const escapedText = escapeXpathString(label);
 	const selector = `//input[@name=(//label[contains(text(),${escapedText})][1]/@for)]`;
@@ -105,46 +41,7 @@ const fillByLabel = async (label, fill ) => {
 	clearInput(el[0])
 	await el[0].click();
 	await el[0].type(fill);
-};
-
-const markByLabel = async (label, un) => {
-	const escapedText = escapeXpathString(label);
-	const selector = `//label[contains(.,${escapedText})]`
-	const page = scope.context.currentPage
-	const el = await page.$x(selector)
-	const handle = el[0]
-	const checkbox = await el[0].$x('./input')
-	const checked = await (await checkbox[0].getProperty('checked')).jsonValue()
-	if ((un && checked) || (!un && !checked)) {
-		await handle.click()
-	}
 }
-
-const selectFormGroupByLabel = async (label) => {
-	const escapedText = escapeXpathString(label);
-	const selector = `//div[contains(@class,'form-group') and contains(.//label,${escapedText})]`
-	const el = await scope.context.currentPage.$x(selector)
-	return el[0]
-}
-
-const selectTypeaheadCloseByLabel = async (label) => {
-	const formGroup = await selectFormGroupByLabel(label)
-	const el = await formGroup.$x(`.//button[contains(@class,'rbt-close')]`)
-	return el[0]
-}
-
-const selectTypeaheadInputByLabel = async (label) => {
-	const formGroup = await selectFormGroupByLabel(label)
-	const el = await formGroup.$x(`.//input[contains(@class,'rbt-input-main')]`)
-	return el[0]
-}
-
-const fillElement = async (el, fill, clear = false ) => {
-	if (clear) await clearInput(el)
-	await el.click()
-	await el.type(fill)
-};
-
 const fillByPlaceholder = async (label, fill, clear = false ) => {
 	const escapedText = escapeXpathString(label);
 	const selector = `//input[contains(@placeholder,${escapedText})]`;
@@ -152,8 +49,12 @@ const fillByPlaceholder = async (label, fill, clear = false ) => {
 	const el = await page.$x(selector);
 	await el[0].click();
 	await el[0].type(fill);
-};
-
+}
+const fillElement = async (el, fill, clear = false ) => {
+	if (clear) await clearInput(el)
+	await el.click()
+	await el.type(fill)
+}
 const fillTypeaheadByLabel = async (label, fill) => {
 	const closeButton = await selectTypeaheadCloseByLabel(label)
 	if (closeButton) await closeButton.click()
@@ -167,11 +68,53 @@ const fillTypeaheadByLabel = async (label, fill) => {
     await waitFor(choice)
     await clickByXPath(choice)
 }
-
-const waitFor = async (selector) => {
-	await scope.context.currentPage.waitFor(selector);
-};
-
+const formatDateForDisplay = (date) => {
+	const str = toLocalDate(date)
+	return `${parseInt(str.substring(5,7))}/${parseInt(str.substring(8,10))}/${str.substring(0,4)}`
+}
+const formatDateForFill = (date) => {
+	const str = toLocalDate(date)
+	return `${str.substring(5,7)}/${str.substring(8,10)}/${str.substring(0,4)}`
+}
+const initPage = async () => {
+	if ( scope.context.currentPage ) return
+	scope.context.currentPage = await scope.browser.newPage();
+	scope.context.currentPage.setViewport({ width: 1280, height: 1024 });
+	scope.context.currentPage.on('error', err => {
+		console.log("browser error: ",err);
+	});
+	scope.context.currentPage.on('pageerror', err => {
+		console.log("browser pageerror: ",err);
+	});
+}
+const loginAs = async (email) => {
+    await clickByText("Login");
+    await fillByLabel("Email",email);
+    await fillByLabel("Password","secret");
+    await clickByText("Login","//button");
+}
+const markByLabel = async (label, un) => {
+	const escapedText = escapeXpathString(label);
+	const selector = `//label[contains(.,${escapedText})]`
+	const page = scope.context.currentPage
+	const el = await page.$x(selector)
+	const handle = el[0]
+	const checkbox = await el[0].$x('./input')
+	const checked = await (await checkbox[0].getProperty('checked')).jsonValue()
+	if ((un && checked) || (!un && !checked)) {
+		await handle.click()
+	}
+}
+const parseInput = (input,display=false) => {
+	switch (input) {
+		case 'tomorrow':
+		case 'today':
+			if (display) return formatDateForDisplay(relativeDate(input))
+			return formatDateForFill(relativeDate(input))
+		default:
+			return input.replace(/"/g,'')
+	}
+}
 const relativeDate = (description) => {
     const relativeDate = new Date()
     switch(description) {
@@ -184,41 +127,76 @@ const relativeDate = (description) => {
 	relativeDate.setHours(8,0,0,0)
 	return relativeDate
 }
-
-const formatDateForFill = (date) => {
-	const str = toLocalDate(date)
-	return `${str.substring(5,7)}/${str.substring(8,10)}/${str.substring(0,4)}`
+const selectFormGroupByLabel = async (label) => {
+	const escapedText = escapeXpathString(label);
+	const selector = `//div[contains(@class,'form-group') and contains(.//label,${escapedText})]`
+	const el = await scope.context.currentPage.$x(selector)
+	return el[0]
 }
-
-const formatDateForDisplay = (date) => {
-	const str = toLocalDate(date)
-	return `${parseInt(str.substring(5,7))}/${parseInt(str.substring(8,10))}/${str.substring(0,4)}`
+const selectTypeaheadCloseByLabel = async (label) => {
+	const formGroup = await selectFormGroupByLabel(label)
+	const el = await formGroup.$x(`.//button[contains(@class,'rbt-close')]`)
+	return el[0]
 }
-
-const shouldSeeErrorWithLabel = async (error,label) => {
-	await waitFor(`//div[contains(@class,'invalid-feedback') and contains(.,'${error}') and ` +
-		`ancestor::div[contains(@class,'form-group') and contains(.//label,'${label}')]]`)
+const selectTypeaheadInputByLabel = async (label) => {
+	const formGroup = await selectFormGroupByLabel(label)
+	const el = await formGroup.$x(`.//input[contains(@class,'rbt-input-main')]`)
+	return el[0]
 }
-
-const parseInput = (input,display=false) => {
-	switch (input) {
-		case 'tomorrow':
-		case 'today':
-			if (display) return formatDateForDisplay(relativeDate(input))
-			return formatDateForFill(relativeDate(input))
-		default:
-			return input.replace(/"/g,'')
-	}
+const shouldBeLoggedInAs = async(email) => {
+	await waitFor("//a[contains(text(),'Logout')]");
+	const user = await User.findOne({email})
+    await shouldSeeText(".navbar",false,`Welcome, ${user.firstName}`);
+    await shouldSeeText(".navbar",false,"Logout");
 }
-
+const shouldSee = async (selector,context) => {
+	await scope.context.currentPage.waitForXPath(selectors[context][selector]);
+} 
 const shouldSeeDefinition = async (dt,dd) => {
 	const q = `//dl/dt[contains(text(),'${dt}')]/following-sibling::dd[contains(text(),'${dd}')]`
 	await waitFor(q)
 }
-
+const shouldSeeErrorWithLabel = async (error,label) => {
+	await waitFor(`//div[contains(@class,'invalid-feedback') and contains(.,'${error}') and ` +
+		`ancestor::div[contains(@class,'form-group') and contains(.//label,'${label}')]]`)
+}
+const shouldSeeText = async (selector, not, expectedText) => {
+	const elementText = await scope.context.currentPage.$eval(selector, el => el.textContent);
+    const containsText = elementText && elementText.replace(/\s+/,' ')
+	const shouldContainText = not ? false : true;
+	if (shouldContainText) {
+		containsText.should.have.string(expectedText)
+	}
+	else {
+		containsText.should.not.have.string(expectedText)
+	}
+}
 const takeScreenshot = async () => {
 	await new Promise(r => setTimeout(r, 200))
 	await scope.context.currentPage.screenshot({path: `sc${sc++}.png`});
+}
+const userExists = async (attr) => {
+	scope.context.user = await scope.factory.create('user',{password: "secret", ...attr});
+}
+const visitExists = async (attr={}) => {
+    scope.context.visit = await scope.factory.create('visit',attr);
+}
+const visitPage = async (path) => {
+	await initPage()
+	const url = scope.host + paths[path];
+	const visit = await scope.context.currentPage.goto(url, {
+		waitUntil: 'networkidle2'
+	});
+	await waitFor('.navbar')
+	return visit;
+}
+const waitForText = async (text, context = "//a") => {
+	const escapedText = escapeXpathString(text);
+	const selector = `${context}[contains(text(), ${escapedText})]`;
+	await scope.context.currentPage.waitForXPath(selector);
+}
+const waitFor = async (selector) => {
+	await scope.context.currentPage.waitFor(selector);
 }
 
 module.exports = {
