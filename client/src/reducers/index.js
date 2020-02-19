1cf0c68b2a5ecc800b32ddc280644dd0b4768681
Ari Epstein <aepstein607@gmail.com>
2020-02-19 08:18:30 -0500

    Add geolocation state management without tests.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
+import geoReducer from './geoReducer'
 import placeReducer from './placeReducer'
 import userReducer from './userReducer'
 import visitReducer from './visitReducer';
@@ -8,6 +9,7 @@ import visitReducer from './visitReducer';
 export default combineReducers({
     auth: authReducer,
     error: errorReducer,
+    geo: geoReducer,
     place: placeReducer,
     user: userReducer,
     visit: visitReducer
