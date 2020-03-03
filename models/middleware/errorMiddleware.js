71f274d989b629e06cde840a445e3c7b383c53c7
Ari Epstein <aepstein607@gmail.com>
2020-03-02 22:28:44 -0500

    Handle mongo error11000 (#35)
    * Add error message parsing to make test pass on older mongo running on travisci.

--- models/middleware/errorMiddleware.js
+++ models/middleware/errorMiddleware.js
@@ -1,15 +1,24 @@
 const ValidationError = require('mongoose/lib/error/validation')
 const ValidatorError = require('mongoose/lib/error/validator')
 
+function keysFromMongoError11000(err) {
+    if (err.keyPattern) return {
+        keys: Object.keys(err.keyPattern),
+        values: Object.keys(err.keyPattern).map(v => err.keyValue[v])
+    }
+    const m = (/index: ([a-zA-Z_]+)_\d+ dup key: { [^:]*: "([^"]+)" }/).exec(err.errmsg)
+    return {keys: [m[1]], values: [m[2]]}
+}
+
 function handleMongoError11000(err,res, next) {
     if (err.name === 'MongoError' && err.code === 11000) {
-        const keys = Object.keys(err.keyPattern)
+        const {keys,values} = keysFromMongoError11000(err)
         const vErr = new ValidationError(this)
         vErr.addError(keys[0],new ValidatorError({
             type: 'duplicateKey',
             path: keys[0],
-            value: err.keyValue[keys[0]],
-            message: `Path \`${keys.join(',')}\` must be unique. Value: \`${keys.map(v => err.keyValue[v]).join(',')}\``
+            value: values[0],
+            message: `Path \`${keys.join(',')}\` must be unique. Value: \`${values.join(',')}\``
         }))
         next(vErr)
     }
