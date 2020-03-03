728caca8f3411eefd3427cd252ac92e1fc6d8c4f
Ari Epstein <aepstein607@gmail.com>
2020-03-02 21:42:54 -0500

    Try to expose cause of the error on travisci

--- models/middleware/errorMiddleware.js
+++ models/middleware/errorMiddleware.js
@@ -3,6 +3,8 @@ const ValidatorError = require('mongoose/lib/error/validator')
 
 function handleMongoError11000(err,res, next) {
     if (err.name === 'MongoError' && err.code === 11000) {
+        console.log(err)
+        console.log(err.keyPattern)
         const keys = Object.keys(err.keyPattern)
         const vErr = new ValidationError(this)
         vErr.addError(keys[0],new ValidatorError({
