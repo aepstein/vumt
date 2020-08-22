0e8cfb1e419c9e453822d0fcd37e7dad1f4188d9
Ari Epstein <aepstein607@gmail.com>
2020-08-21 22:48:54 -0400

    PUT /api/districts/:districtId

--- test/api/districts.js
+++ test/api/districts.js
@@ -59,4 +59,62 @@ describe.only('/api/districts',() => {
             await errorNoToken(res)
         })
     })
+    describe.only('PUT /api/districts/:districtId', async () => {
+        const action = async (district,props,auth) => {
+            const res = chai.request(server).put('/api/districts/' + district._id).send(props)
+            if (auth) res.set('x-auth-token',auth.body.token)
+            return res
+        }
+        it('should save for authorized user with valid attributes', async () => {
+            const district = await factory.create('district')
+            const auth = await withAuth({roles: ['admin']})
+            attr = {
+                name: 'Different District',
+                boundaries: {
+                    "type": "Polygon",
+                    "coordinates": [[
+                        [
+                        -73.97506713867186,
+                        44.171123644193784
+                        ],
+                        [
+                        -74.02347564697266,
+                        44.13639184602692
+                        ],
+                        [
+                        -73.95515441894531,
+                        44.169892369723506
+                        ],
+                        [
+                        -73.97506713867186,
+                        44.171123644193784
+                        ]
+                    ]]
+                }
+            }
+            const res = await action(district,attr,auth)
+            res.should.have.status(200)
+            res.body.should.be.an('object')
+            res.body.should.have.a.property('name').eql(attr.name)
+            res.body.boundaries.coordinates[0][0].should.have.deep.members(attr.boundaries.coordinates[0][0])
+        })
+        it('should return an error for an invalid submission', async () => {
+            const district = await factory.create('district')
+            const auth = await withAuth({roles: ['admin']})
+            const attr = await validDistrict({name: null})
+            const res = await action(district,attr,auth)
+            errorPathRequired(res,'name')
+        })
+        it('should deny an unprivileged user', async () => {
+            const district = await factory.create('district')
+            const auth = await withAuth()
+            const res = await action(district,{},auth)
+            await errorMustHaveRoles(res,['admin'])
+        })
+        it('should deny without authentication', async() => {
+            const district = await factory.create('district')
+            const res = await action(district,{})
+            await errorNoToken(res)
+        })
+    })
 })
